36ffd1b98bd6811d3f790936cebdf1f586c3b5dd
Ansh <159460386+HiGautamAG@users.noreply.github.com>
2024-08-12 20:28:44 +0530

    Update instance.py

--- instance.py
+++ instance.py
@@ -1,4 +1,13 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
 
+    def bark(self):
+        return f"{self.name} says woof!"
 
+# Create an instance of the class
+my_dog = Dog(name="Buddy", breed="Golden Retriever")
 
-       
+# Use the instance
+print(my_dog.bark())  # Output: Buddy says woof!
