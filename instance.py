ea448b0787046797fecf72ec7047a08b830396a1
Ansh <159460386+HiGautamAG@users.noreply.github.com>
2024-08-12 20:17:46 +0530

    Update instance.py

--- instance.py
+++ instance.py
@@ -1,14 +1,3 @@
-# instance is an object of a class  
-
-class Student:
-    
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
-        print("constructor called")
-        
-s1 = Student("Ansh", 90)
-print(s1.name, s1.marks)
 
 
 
