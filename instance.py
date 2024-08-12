61ac96e2cbc5c258a4214be50b707845a31aed33
Ansh <159460386+HiGautamAG@users.noreply.github.com>
2024-08-12 20:06:37 +0530

    Update instance.py

--- instance.py
+++ instance.py
@@ -1,15 +1,15 @@
 # instance is an object of a class  
 
-
 class Student:
-    college_name ="BBDU"
-    def _init_(self, name, marks):
+    
+    def __init__(self, name, marks):
         self.name = name
         self.marks = marks
         print("constructor called")
         
 s1 = Student("Ansh", 90)
 print(s1.name, s1.marks)
+
 
 
        
